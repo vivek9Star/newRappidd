eaf7e646ba37d59fc1f4400238995a1c35c90657
vivekNinestars <vivek.r@ninestars.in>
2024-01-22 11:12:00 +0530

    newReleases

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,11 +10,10 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:rappid_poc/main.dart';
 
 
-
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget( MyApp());
+    await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
